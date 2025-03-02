7e83f6fc66b0142a1f76e19e3fa039ddc3f2042e
Nasser mostafa <nsasa4289@gmail.com>
2025-03-02 16:49:52 +0200

    Add files via upload

---
+++ RESTFUL APIs/business Layer/FactoriesDP/LoginFactory.cs
@@ -0,0 +1,29 @@
+﻿using StoreBusinessLayer.Interfaces;
+using StoreBusinessLayer.Users;
+using StoreDataAccessLayer;
+
+namespace StoreBusinessLayer.FactoriesDP
+{
+    public class LoginFactory
+    {
+        private Dictionary<string, ILogin> _logins;
+        public LoginFactory(AppDbcontext dbContext)
+        {
+            _logins = new Dictionary<string, ILogin>
+            {
+                { "google", new GoogleLogin(dbContext) },
+               { "online store", new OnlineStoreLogin(dbContext) }
+
+            };
+        }
+        public ILogin GetLoginProvider(string providerName)
+        {
+            if (_logins.ContainsKey(providerName.ToLower()))
+            {
+                return _logins[providerName.ToLower()];
+            }
+
+            throw new Exception($"Login provider '{providerName}' is not supported.");
+        }
+    }
+}
---
+++ RESTFUL APIs/business Layer/FactoriesDP/NotificationFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using StoreBusinessLayer.Interfaces;
+using StoreBusinessLayer.NotificationServices;
+using StoreBusinessLayer.Users;
+using StoreDataAccessLayer;
+
+namespace StoreBusinessLayer.FactoriesDP
+{
+    public class NotificationFactory
+    {
+        private Dictionary<string, INotifications> _logins;
+        public NotificationFactory()
+        {
+            _logins = new Dictionary<string, INotifications>
+            {
+                { "gmail",  (new GmailServes()) },
+       
+
+            };
+        }
+        public INotifications GetNotificationProvider(string providerName)
+        {
+            if (_logins.ContainsKey(providerName.ToLower()))
+            {
+                return _logins[providerName.ToLower()];
+            }
+
+            throw new Exception($"Login provider '{providerName}' is not supported.");
+        }
+    }
+}
