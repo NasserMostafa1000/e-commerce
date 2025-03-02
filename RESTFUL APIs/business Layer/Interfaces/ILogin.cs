786db4d8f301a0c80d64fd7c04aafe346afe1e05
Nasser mostafa <nsasa4289@gmail.com>
2025-03-02 16:54:34 +0200

    Add files via upload

---
+++ RESTFUL APIs/business Layer/Interfaces/ILogin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StoreBusinessLayer.Users;
+using StoreDataAccessLayer.Entities;
+
+namespace StoreBusinessLayer.Interfaces
+{
+    public  interface ILogin
+    {
+        Task<StoreDataAccessLayer.Entities.Users> LoginWithProviderAsync(string Email, string Token, string Password);
+    }
+
+}
---
+++ RESTFUL APIs/business Layer/Interfaces/INotifications.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreBusinessLayer.Interfaces
+{
+    public interface INotifications
+    {
+        Task SendNotification(string Subject, string Body, string UserProviderIdentifier); 
+    }
+}
